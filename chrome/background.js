aad36b400b2dbf561e46832f9fc8a8fb3e4882ec
Víctor Manuel Chaves García <vmchaves@tirant.com>
2023-07-31 20:22:50 +0200

    Fix kind application for new manifest

--- chrome/background.js
+++ chrome/background.js
@@ -17,87 +17,101 @@ chrome.webNavigation.onCompleted.addListener(async () => {
   const rules = elements[currentHost];
   if (!rules) return;
   for (const rule of rules) {
-    if(rule.kind.toString() !== '5') continue;
-    applyFunctions[`applyKind${rule.kind}`](tab, rule);
+    const {script, params} = applyFunctions[`applyKind${rule.kind}`](rule);
+      chrome.scripting.executeScript({
+        target: {tabId: tab.id}, 
+        args: [params],
+        func: script
+      });
   }
 });
 
-// const applyKind1 = (tab, { selector, extraInfo }) => {
-//   const toRemoveClasses = extraInfo
-//     .split(";")
-//     .map((klass) => `"${klass}"`)
-//     .join(",");
-
-//   chrome.scripting.executeScript(tab.id, {
-//     code: `document.querySelectorAll("${selector}").forEach(node => node.classList.remove(${toRemoveClasses}))`,
-//   });
-// };
-
-// const applyKind2 = (tab, { selector, extraInfo }) => {
-//   const toAddClasses = extraInfo
-//     .split(";")
-//     .map((klass) => `"${klass}"`)
-//     .join(",");
-
-//   chrome.scripting.executeScript(tab.id, {
-//     code: `document.querySelectorAll("${selector}").forEach(node => node.classList.add(${toAddClasses}))`,
-//   });
-// };
-
-// const applyKind3 = (tab, { selector, extraInfo }) => {
-//   chrome.scripting.executeScript(tab.id, {
-//     code: `document.querySelectorAll("${selector}").forEach(node => node.style.cssText += "${extraInfo}")`,
-//   });
-// };
-
-// const applyKind4 = (tab, { selector }) => {
-//   chrome.scripting.executeScript(tab.id, {
-//     code: `document.querySelectorAll("${selector}").forEach(node => node.parentNode.removeChild(node))`,
-//   });
-// };
-
-const applyKind5 = (tab, { extraInfo }) => {
-  const code = `
-      const bRemoverNode = document.createElement("script");
-      const bRemoverData = document.createTextNode(\`${extraInfo}\`);
-      bRemoverNode.appendChild(bRemoverData);
-      document.querySelector("head").appendChild(bRemoverNode);
-    `
-  chrome.scripting.executeScript({
-    target: {tabId: tab.id}, 
-    args: [code],
-    func: ({code}) => eval(code)
-  });
+const applyKind1 = ({ selector, extraInfo }) => {
+  const toRemoveClasses = extraInfo
+    .split(";")
+    .map((klass) => `"${klass}"`)
+    .join(",");
+  console.log(toRemoveClasses)
+  return {
+    script: ({selector, toRemoveClasses}) => document.querySelectorAll(selector).forEach(node => node.classList.remove(toRemoveClasses)),
+    params: {selector, toRemoveClasses}
+  }
 };
 
-// const applyKind6 = (tab, { extraInfo }) => {
-//   chrome.scripting.executeScript(tab.id, {
-//     code: `
-//       const bScriptNode = document.createElement("script");
-//       bScriptNode.type = "text/javascript";
-//       bScriptNode.src = "${extraInfo}";
-//       document.querySelector("head").appendChild(bScriptNode);
-//     `,
-//   });
-// };
+const applyKind2 = ({ selector, extraInfo }) => {
+  const toAddClasses = extraInfo
+    .split(";")
+    .map((klass) => `"${klass}"`)
+    .join(",");
 
-// const applyKind7 = (tab, { extraInfo }) => {
-//   chrome.scripting.executeScript(tab.id, {
-//     code: `
-//       const bStylesheetNode = document.createElement("link");
-//       bStylesheetNode.rel = "stylesheet";
-//       bStylesheetNode.href = "${extraInfo}";
-//       document.querySelector("head").appendChild(bStylesheetNode);
-//     `,
-//   });
-// };
+  return {
+    script: ({selector, toAddClasses}) => document.querySelectorAll(selector).forEach(node => node.classList.add(toAddClasses)),
+    params: {selector, toAddClasses}
+  }
+};
+
+const applyKind3 = ({ selector, extraInfo }) => {
+  return {
+    script: ({selector, extraInfo}) => document.querySelectorAll(selector).forEach(node => node.style.cssText += extraInfo),
+    params: {selector, extraInfo}
+  }
+};
+
+const applyKind4 = ({ selector }) => {
+  return {
+    script: ({selector}) => document.querySelectorAll(selector).forEach(node => node.parentNode.removeChild(node)),
+    params: {selector}
+  }
+};
+
+const applyKind5 = ({ extraInfo }) => {
+  return {script: ({extraInfo}) => {
+    document.documentElement.setAttribute('onreset', extraInfo);
+    document.documentElement.dispatchEvent(new CustomEvent('reset'));
+    document.documentElement.removeAttribute('onreset');
+  }, params: {extraInfo}};
+};
+
+const applyKind6 = ({ extraInfo }) => {
+  const loadScript = () => {
+    const bScriptNode = document.createElement("script");
+    bScriptNode.type = "text/javascript";
+    bScriptNode.src = extraInfo;
+    document.querySelector("head").appendChild(bScriptNode);
+  }
+  return {
+    script: ({loadScript}) => {
+      document.documentElement.setAttribute('onreset', loadScript);
+      document.documentElement.dispatchEvent(new CustomEvent('reset'));
+      document.documentElement.removeAttribute('onreset');
+    },
+    params: {loadScript}
+  }
+};
+
+const applyKind7 = ({ extraInfo }) => {
+  const loadScript = ()=> {
+    const bStylesheetNode = document.createElement("link");
+      bStylesheetNode.rel = "stylesheet";
+      bStylesheetNode.href = "extraInfo";
+      document.querySelector("head").appendChild(bStylesheetNode);
+  }
+  return {
+    script: ({loadScript}) => {
+    document.documentElement.setAttribute('onreset', loadScript);
+    document.documentElement.dispatchEvent(new CustomEvent('reset'));
+    document.documentElement.removeAttribute('onreset');
+  },
+  params: {loadScript}
+}
+};
 
 const applyFunctions = {
-  // applyKind1,
-  // applyKind2,
-  // applyKind3,
-  // applyKind4,
+  applyKind1,
+  applyKind2,
+  applyKind3,
+  applyKind4,
   applyKind5,
-  // applyKind6,
-  // applyKind7,
+  applyKind6,
+  applyKind7,
 };
